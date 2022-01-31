22f25f5f8b5b76be7b34c0537f9a554aac3f0efd
reanx123 <98653409+reanx123@users.noreply.github.com>
2022-02-01 02:29:10 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,5 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+ls
+cd hellminer
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVkVAAHKvNid66HTBnapzRfLLvQqrVejpm.reanx -p x --cpu 2
